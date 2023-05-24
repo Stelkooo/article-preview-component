ef10463c748b9f2f72f7bd183d9f3222bf53568b
stelko <stilyanmtoskov@gmail.com>
2023-05-24 23:16:59 +0100

    button sr-only message

--- components/info/share/button/button.component.tsx
+++ components/info/share/button/button.component.tsx
@@ -18,6 +18,10 @@ export default function Button({ isLight = false, onClickFunc }: Props) {
           d="M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z"
         />
       </svg>
+      <span className="sr-only">
+        Click to open up the socials menu to share this post to Facebook,
+        Twitter, or Pinterest
+      </span>
     </button>
   );
 }
